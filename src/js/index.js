ba04e495673aa9fa0d701646e833c0ffab91899d
Michael Sullivan <sullivanmichael344@gmail.com>
2024-10-17 22:25:19 -0400

    Feat: Create intitial layout for mobile design

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import '../css/style.css'
+import selfie from "../img/selfie.jpg"
+import githubLogo from "../img/github-original.svg"
+import linkedinLogo from "../img/linkedin-plain.svg"
+import nycPhoto from "../img/NYC.jpg"
+
+let headshot = document.querySelector(".headshot")
+let githubIcon = document.querySelectorAll(".github-icon")
+let linkedinIcon = document.querySelectorAll(".linkedin-icon")
+let nyImage = document.querySelector(".ny-photo")
+headshot.src = selfie
+nyImage.src = nycPhoto
+
+githubIcon.forEach((icon) =>{
+    icon.src = githubLogo
+})
+linkedinIcon.forEach((icon) =>{
+    icon.src = linkedinLogo
+})
